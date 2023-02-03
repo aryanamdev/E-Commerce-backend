a819c1a2f41d8f93c8f88e8fabd220415afef611
Aryan Namdev <aryanamdev08@gmail.com>
2023-02-03 22:34:45 +0530

    added user model 01

--- models/User.schema.js
+++ models/User.schema.js
@@ -1,10 +1,38 @@
 const mongoose = require("mongoose");
+const { authRoles } = require("../utils/authRoles");
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    require: [true, "Field is required"],
-    maxLength: [50, "String can be only 25 Chars in length"],
-    trim: true,
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: [true, "Name is required"],
+      maxLength: [50, "String can be only 25 Chars in length"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: [8, "Password must be at least 8 characters long"],
+
+      // this field will not come when we send it to the frontend.
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: Object.values(authRoles),
+      default: authRoles.USER,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date,
   },
-});
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", UserSchema);
