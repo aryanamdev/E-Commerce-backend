6f3d2f22aadde3fb57b3e7fc2e266194a1ef0925
Aryan Namdev <aryanamdev08@gmail.com>
2023-02-08 13:27:07 +0530

    Product schema added

--- models/products.schema.js
+++ models/products.schema.js
@@ -4,7 +4,37 @@ const productSchema = new mongoose.Schema(
   {
     name: {
       type: String,
+      trim: true,
       required: [true, "Name is a required field!"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Please provide a product price"],
+      required: [5, "Product price should not be more than 5 digits"],
+    },
+    description: {
+      type: String,
+      // Use some form of editor - personal assignment
+    },
+    photos: [
+      {
+        secure_url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    collectionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Collection",
     },
   },
   {
